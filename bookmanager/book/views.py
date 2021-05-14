ecd9494c8d3975d05cf634bdccd1f6a9bffe697f
itfeiyu <1345796041@qq.com>
2021-05-14 16:02:37 +0800

    模板

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -15,7 +15,19 @@ from django.http import HttpResponse
 
 #我们期望用户输入 http://127.0.0.1:8000/index 来访问视图函数
 def index(request):
-    return HttpResponse('OK')
+    # return HttpResponse('OK')
 
+    #render  渲染模板
+    #request, template_name, context=None
+    #request  请求
+    #template_name  模板名字
+    #context=None
+
+    #模拟数据查询
+    context={
+        'name':'马上双十一，点击有惊喜'      #context理解为将视图中的数据传递给HTML（模板）
+                                        #HTML（模板）采用{{ 变量 }} 形式来展示数据
+    }
+    return render(request, 'book/index.html', context=context)
 
 
